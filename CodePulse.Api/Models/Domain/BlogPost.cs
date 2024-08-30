a9cd742bfcaad0ce1828b6589b9af39f9ae2aa03
SJAR03 <sayerdisrodriguez@gmail.com>
2024-08-30 16:00:22 -0600

    Add project files.

---
+++ CodePulse.Api/Models/Domain/BlogPost.cs
@@ -0,0 +1,15 @@
+﻿namespace CodePulse.Api.Models.Domain
+{
+    public class BlogPost
+    {
+        public Guid Id { get; set; }
+        public string Title { get; set; }
+        public string ShortDescription { get; set; }
+        public string Content { get; set; }
+        public string FeaturedImageUrl{ get; set; }
+        public string UrlHandle { get; set; }
+        public DateTime DateCreated { get; set; }
+        public string Author { get; set; }
+        public bool IsVisible { get; set; }
+    }
+}
