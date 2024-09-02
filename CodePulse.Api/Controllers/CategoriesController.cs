02b8ad2cb33e3a1a3b961dc6076d096b2ace3c3a
Sergio Ayerdis <72574282+SJAR03@users.noreply.github.com>
2024-09-02 00:20:57 -0600

    Post category action method

---
+++ CodePulse.Api/Controllers/CategoriesController.cs
@@ -0,0 +1,46 @@
+﻿using CodePulse.Api.Data;
+using CodePulse.Api.Models.Domain;
+using CodePulse.Api.Models.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CodePulse.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public CategoriesController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        // POST: api/Categories
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryRequestDto request)
+        {
+            // Map DTO to domain model
+            var category = new Category
+            {
+                Name = request.Name,
+                UrlHandle = request.UrlHandle
+            };
+
+            await dbContext.Categories.AddAsync(category);
+            await dbContext.SaveChangesAsync();
+
+            // Domain model to DTO
+            var response = new CategoryDto
+            {
+                Id = category.Id,
+                Name = category.Name,
+                UrlHandle = category.UrlHandle
+            };
+
+            return Ok(response);
+
+        }
+    }
+}
